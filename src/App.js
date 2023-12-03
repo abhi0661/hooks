3e3184a8b883b8d934d36cb4ee136258cc9ecbfd
Abhimaheshwari1 <80679973+Abhimaheshwari1@users.noreply.github.com>
2023-12-03 16:54:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
- import Resturant from "./component/Basics/Resturant";
+//  import Resturant from "./component/Basics/Resturant";
 // import UseState from "./component/Hooks/useState";
 // import UseEffect from "./component/Hooks/useEffect";
- import UseReducer from "./component/Hooks/useReducer";
+ import UseReducer from "./Hooks/useReducer";
 
 const App = () => {
   return <UseReducer />;
